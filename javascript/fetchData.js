998aa7e95412fd9f7fece0ef822622e164237d26
qianli <121330735+QianLI52525@users.noreply.github.com>
2023-12-12 19:33:31 +0800

    Update fetchData.js

--- javascript/fetchData.js
+++ javascript/fetchData.js
@@ -40,3 +40,36 @@ function traverseFolder(folderPath) {
                 document.getElementById('loadedFolder').innerHTML += '文件获取失败';
             });
     }
+
+function fetchFile(folderPath){
+    fetch('files/')
+        .then(response => response.text())
+        .then(htmlContent => {
+            // 创建一个临时div，用于解析HTML内容
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = htmlContent;
+
+            // 提取所有<a>标签并生成文件链接
+            const fileLinks = tempDiv.querySelectorAll('a');
+            const fileLinksDiv = document.getElementById('fileLinks');
+
+            // 如果有文件链接，则将它们插入到页面中
+            if (fileLinks.length > 0) {
+                fileLinks.forEach(link => {
+                    const fileLink = document.createElement('a');
+                    fileLink.href = `files/${link.getAttribute('href')}`;
+                    fileLink.textContent = link.textContent;
+                    fileLink.setAttribute('target', '_blank'); // 在新窗口中打开链接
+                    fileLinksDiv.appendChild(fileLink);
+                    fileLinksDiv.appendChild(document.createElement('br')); // 添加换行
+                });
+            } else {
+                fileLinksDiv.textContent = '文件提取失败';
+            }
+        })
+        .catch(error => {
+            console.error('Error loading file links:', error);
+            // 如果发生错误，将错误消息插入到页面中
+            document.getElementById('fileLinks').textContent = '文件提取失败';
+        });
+}
