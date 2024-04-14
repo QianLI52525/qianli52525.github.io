ea574e966f1155e7dfb11aa5eb6c0a587b1542a0
qianli <121330735+QianLI52525@users.noreply.github.com>
2024-04-14 22:26:27 +0800

    更新 CountdownAutoSwitch.js

--- javascript/CountdownAutoSwitch.js
+++ javascript/CountdownAutoSwitch.js
@@ -1,9 +1,9 @@
-function countStartStopSwitch(t1, t2, w){
+function countStartStopSwitch(t1, t2, w, h){
     var timeStart = new Date(t1).getTime();
     var timeStop = new Date(t2).getTime();
     var timeNow = new Date().getTime();
     if (timeNow < timeStart) {
-        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: " + w + "%; height:100px; margin: 0px; display: block; border: 0px;\"></iframe></center>");
+        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: " + w + "; height:" + h + "; margin: 0px; display: block; border: 0px;\"></iframe></center>");
     } else if (timeNow > timeStop) {
         document.write("<span class=\"subtitle2\">已结束</span>");
     } else {
@@ -11,12 +11,12 @@ function countStartStopSwitch(t1, t2, w){
     }
 }
 
-function countStartDurationSwitch(t1, duration, w){
+function countStartDurationSwitch(t1, duration, w, h){
     var timeStart = new Date(t1).getTime();
     var timeStop = timeStart + duration * 60 * 1000;
     var timeNow = new Date().getTime();
     if (timeNow < timeStart) {
-        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: " + w + "%; height:100px; margin: 0px; display: block; border: 0px;\"></iframe></center>");
+        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: " + w + "; height:" + h + "; margin: 0px; display: block; border: 0px;\"></iframe></center>");
     } else if (timeNow > timeStop) {
         document.write("<span class=\"subtitle2\">已结束</span>");
     } else {
