593a15d4d719cb7959bd3fcf16d128fd581fea34
qianli <121330735+QianLI52525@users.noreply.github.com>
2024-04-14 22:18:07 +0800

    更新 CountdownAutoSwitch.js

--- javascript/CountdownAutoSwitch.js
+++ javascript/CountdownAutoSwitch.js
@@ -1,9 +1,22 @@
-function countStartStopSwitch_half(t1, t2){
+function countStartStopSwitch(t1, t2, w){
     var timeStart = new Date(t1).getTime();
     var timeStop = new Date(t2).getTime();
     var timeNow = new Date().getTime();
     if (timeNow < timeStart) {
-        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: 100%; height:100; margin: 0px; display: block; border: 0px;\"></iframe></center>");
+        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: " + w + "%; height:100px; margin: 0px; display: block; border: 0px;\"></iframe></center>");
+    } else if (timeNow > timeStop) {
+        document.write("<span class=\"subtitle2\">已结束</span>");
+    } else {
+        document.write("<span class=\"subtitle2\">进行中</span>");
+    }
+}
+
+function countStartDurationSwitch(t1, duration, w){
+    var timeStart = new Date(t1).getTime();
+    var timeStop = timeStart + duration * 60 * 1000;
+    var timeNow = new Date().getTime();
+    if (timeNow < timeStart) {
+        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: " + w + "%; height:100px; margin: 0px; display: block; border: 0px;\"></iframe></center>");
     } else if (timeNow > timeStop) {
         document.write("<span class=\"subtitle2\">已结束</span>");
     } else {
