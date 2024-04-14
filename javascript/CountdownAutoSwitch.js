4a751cac61ab7bbc97f11ebb644286860cbc16e1
qianli <121330735+QianLI52525@users.noreply.github.com>
2024-04-14 20:59:48 +0800

    更新 CountdownAutoSwitch.js

--- javascript/CountdownAutoSwitch.js
+++ javascript/CountdownAutoSwitch.js
@@ -1,3 +1,12 @@
-function countSwitch(t1, t2){
-  
+function countStartStopSwitch(t1, t2){
+    var timeStart = new Date(t1).getTime();
+    var timeStop = new Date(t2).getTime();
+    var timeNow = new Date().getTime();
+    if (timeNow < timeStart) {
+        document.write("<center><iframe id=\"online-alarm-kur-iframe\" src=\"https://embed-countdown.onlinealarmkur.com/zh-cn/#" + t1 + "@Asia%2FHong_Kong\" style=\"width: 100%; height:100; margin: 0px; display: block; border: 0px;\"></iframe></center>");
+    } else if (timeNow > timeStop) {
+        document.write("<span class=\"subtitle2\">已结束</span>");
+    } else {
+        document.write("<span class=\"subtitle2\">进行中</span>");
+    }
 }
