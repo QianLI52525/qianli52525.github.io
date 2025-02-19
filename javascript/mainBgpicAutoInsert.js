832672b36f9f6733e9e780548d552300c26ecdce
qianli <121330735+QianLI52525@users.noreply.github.com>
2025-02-19 22:54:00 +0800

    Update mainBgpicAutoInsert.js

--- javascript/mainBgpicAutoInsert.js
+++ javascript/mainBgpicAutoInsert.js
@@ -3,42 +3,43 @@ var imgh = 1;
 var imgwhratio = 1;
 var img = new Image();
 
+// 默认图片 URL
+const defaultImgUrl = "https://qianli52525.github.io/bgpic/defaultBgpic.jpeg";
+
 function displayBgpic(imgsrc) {
-    img.src = imgsrc;
-    img.onerror = function() {
-        console.warn("Image not found. Using default image.");
-        img.src = "https://qianli52525.github.io/bgpic/defaultBgpic.jpeg";
-    };
+    img.src = imgsrc; // 设置图片源
     img.onload = function() {
         imgw = img.width;
         imgh = img.height;
         imgwhratio = imgw / imgh;
         updateDisplay();
     };
+    img.onerror = function() {
+        // 如果图片加载失败，切换到默认图片
+        displayBgpic(defaultImgUrl);
+    };
 }
 
 let logTimeoutId;
-let imgSelected = "https://qianli52525.github.io/bgpic/defaultBgpic.jpeg";
 
 function updateDisplay() {
     var w = document.documentElement.clientWidth;
     var h = document.documentElement.clientHeight;
     var whratio = w / h;
-    
+
     if (imgwhratio >= whratio) {
-        document.getElementById("bgpic").innerHTML = "<img class=\"bgpich\" src=\"" + img.src +  "\">";
+        document.getElementById("bgpic").innerHTML = `<img class="bgpich" src="${img.src}">`;
     } else {
-        document.getElementById("bgpic").innerHTML = "<img class=\"bgpicw\" src=\"" + img.src +  "\">";
-    } 
-    
+        document.getElementById("bgpic").innerHTML = `<img class="bgpicw" src="${img.src}">`;
+    }
+
     clearTimeout(logTimeoutId);
     logTimeoutId = setTimeout(function() {
-        console.log("图片： ", imgwhratio, "窗口：", whratio);
+        console.log("图片宽高比：", imgwhratio, "窗口宽高比：", whratio);
     }, 200);
 }
 
-displayBgpic(imgSelected);
-
+// 监听窗口大小变化
 window.addEventListener("resize", function() {
     updateDisplay();
 });
